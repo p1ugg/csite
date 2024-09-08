7ced79fea6de472d4469a2c4522097b06cc7beac
Yaroslav <yarik.aimov2@gmail.com>
2024-09-08 23:35:17 +0300

    with success wnd and modified directions

--- main.py
+++ main.py
@@ -5,27 +5,28 @@ logging.basicConfig(level=logging.DEBUG)
 
 application = Flask(__name__)
 
+# @application.route('/')
+# def home():
+#     return render_template('index.html')
+
 @application.route('/')
 def home():
-    return render_template('index.html')
+    merchant_order_id = request.args.get('MERCHANT_ORDER_ID')
+    intid = request.args.get('intid')
+
+    logging.debug(f'MERCHANT_ORDER_ID: {merchant_order_id}, intid: {intid}')
+    print(merchant_order_id, intid)
+
+    return render_template('index.html', merchant_order_id=merchant_order_id, intid=intid)
 
 @application.route('/contact_us')
 def contact_us():
     return render_template('contact_info.html')
 
-# @application.route('/notification')
-# def notification():
-#     return '<html><body><h1>YES</h1></body></html>'
 
-# @application.route('/notification', methods=['POST'])
-# def notification():
-#     MERCHANT_ORDER_ID = request.form.get('MERCHANT_ORDER_ID') 
-#     AMOUNT = request.form.get('AMOUNT')  
-
-#     print(f"Payment: {MERCHANT_ORDER_ID}")
-#     print(f"Payment Amount: {AMOUNT}")
-
-#     return "Notification received", 200
+@application.route('/success')
+def success():
+    return render_template('success.html')
 
 
 @application.route('/notification', methods=['GET', 'POST'])
@@ -36,16 +37,15 @@ def notification():
     amount = request.form.get('AMOUNT')
     currency = request.form.get('CUR_ID')
     received_sign = hashlib.md5(f"{order_id}:{amount}:{secret_word}:{currency}".encode()).hexdigest()
-    print("111")
     if sign == received_sign:
         print(f"Payment: {order_id}")
         print(f"Payment Amount: {amount}")
 
         email = order_id 
 
-        return "Notification received", 200
+        return "OK", 200
     else:
-        return "Invalid signature", 400
+        return "FAIL", 400
 
 @application.route('/buy_course', methods=['POST'])
 def buy_course():
@@ -53,7 +53,7 @@ def buy_course():
     username = request.form.get('username')
     print(email, username)
 
-    payment_url = "https://pay.freekassa.com/?oa=499&o=%D0%9A%D1%83%D1%80%D1%81%20%D0%A1%D0%9C%D0%9C%20%D0%9C%D0%B5%D0%BD%D0%B5%D0%B4%D0%B6%D0%B5%D1%80&s=9307cbf9e5c4537c608fe920422f6391&currency=RUB&m=53528"
+    payment_url = "https://pay.freekassa.com/?m=53528&oa=1000&o=2&s=146bc3db76fa06bd969b0dc2c7c6b7de&currency=RUB"
     return redirect(payment_url)
 
 if __name__ == '__main__':
